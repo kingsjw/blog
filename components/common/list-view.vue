ab55f02ab20517a1ad5c48b0f55d96e1f893572d
kingsjw <kingsjw7@gmail.com>
2020-05-18 17:27:26 +0900

    add list-view loading ui

--- components/common/list-view.vue
+++ components/common/list-view.vue
@@ -1,9 +1,14 @@
 <template>
 	<div class="list-view" :class="$store.state.common.isMobile ? 'mobile' : ''">
-		<div
-			v-if="!editMode"
-			class="contents markdown-body" v-html="data.contents"
-		></div>
+		<div v-if="!editMode">
+			<listViewLoading
+				v-if="loading"
+			></listViewLoading>
+			<div
+				v-else
+				class="contents markdown-body" v-html="data.contents"
+			></div>
+		</div>
 		<editForm
 			v-else
 			:data="data"
@@ -24,24 +29,29 @@
 <script>
   import 'github-markdown-css/github-markdown.css';
 	import Firebase from 'firebase';
-	import editForm from './formControl';
+	import editForm from './formControl.vue';
+	import listViewLoading from '../loading/list-view-loading.vue';
 
 	export default {
 		props: ['flag'],
 		components: {
 			editForm,
+			listViewLoading,
 		},
 		data() {
 			return {
 				data: '',
+				loading: false,
 				editMode: false,
 			};
 		},
 		async mounted() {
 			const db = Firebase.firestore();
+			this.loading = true;
 			const doc = await db.collection(this.flag).doc(this.$route.query.id).get();
       const data = Object.assign({ id: doc.id }, doc.data());
-      this.data = data;
+			this.data = data;
+			this.loading = false;
 		},
 	};
 </script>
