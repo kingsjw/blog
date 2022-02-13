591c49ecac2da2c9cea48d6e0832f3d4e00e4809
kingsjw <kingsjw7@gmail.com>
2022-02-13 21:03:37 +0900

    빌드 파일 배포

--- docs/_nuxt/static/1641725103/tech/2021-end/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1641725103",layout:"default",error:a,state:{device:{isMobile:false}},serverRendered:true,routePath:"\u002Ftech\u002F2021-end",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"17f75c4e"}}}}(null));
