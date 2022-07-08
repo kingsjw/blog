975981ce21de306bb72cf853fbd5266aee8f3fa2
kingsjw <kingsjw7@gmail.com>
2022-07-08 17:33:54 +0900

    fix: page scrollTo 시 queryselector가 숫자로 시작하는 element id값을 인식하지 못하는 오류

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -4,10 +4,15 @@ export default function (to, from, savedPosition) {
   if (savedPosition) {
     return savedPosition;
   }
-  if (to.hash) {
-    const tatgetY = document.querySelector(decodeURIComponent(to.hash)).getBoundingClientRect().top;
+
+  const targetElementId = to.hash.split('#')[1];
+
+  if (targetElementId) {
+    const tatgetY = document.querySelector(`[id='${to.hash.split('#')[1]}']`).getBoundingClientRect().top;
     const docY = document.documentElement.getBoundingClientRect().top;
+
     return window.scrollTo({ top: tatgetY - docY - headerHeight, behavior: 'smooth' });
   }
+
   return { x: 0, y: 0 };
 }
