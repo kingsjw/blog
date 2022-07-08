4ab6bf4f51914abad46a605defcae5de4de80450
kingsjw <kingsjw7@gmail.com>
2022-07-08 18:47:23 +0900

    빌드 파일 배포

---
+++ docs/_nuxt/static/1657273622/movie/ForrestGump/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1657273622",layout:"default",error:a,state:{device:{isMobile:false}},serverRendered:true,routePath:"\u002Fmovie\u002FForrestGump",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"42b979a4"}}}}(null));
