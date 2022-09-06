2a32782100a32d9fb4b89c090937c560e485c7f3
kingsjw <kingsjw7@gmail.com>
2022-09-06 16:50:55 +0900

    빌드 파일 배포

---
+++ docs/_nuxt/static/1662450616/profile/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1662450616",layout:"default",error:a,state:{device:{isMobile:false}},serverRendered:true,routePath:"\u002Fprofile",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7bba38df"}}}}(null));
