347ebe85885870d48b2d613110a5116bc7e12e70
kingsjw <kingsjw7@gmail.com>
2023-07-25 00:00:35 +0900

    빌드파일배포

--- docs/_nuxt/static/1689840989/movie/MillionDollarBaby/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1689840989",layout:"default",error:a,state:{device:{isMobile:false}},serverRendered:true,routePath:"\u002Fmovie\u002FMillionDollarBaby",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"59ac5181"}}}}(null));
