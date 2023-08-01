cef3a206f4a38b74379da76a992741e792806313
kingsjw <kingsjw7@gmail.com>
2023-08-02 01:47:19 +0900

    빌드 파일 배포

--- docs/_nuxt/static/1690895311/movie/joker2019/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1690895311",layout:"default",error:a,state:{device:{isMobile:false}},serverRendered:true,routePath:"\u002Fmovie\u002Fjoker2019",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"54829f68"}}}}(null));
