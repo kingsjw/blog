89f643bfda3fbffde5174cd0c81b412751cfc893
kingsjw <kingsjw7@gmail.com>
2023-09-11 22:13:19 +0900

    빌드 파일 배포

--- docs/_nuxt/static/1692170537/movie/pianist/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1692170537",layout:"default",error:a,state:{device:{isMobile:false}},serverRendered:true,routePath:"\u002Fmovie\u002Fpianist",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"61f1b05f"}}}}(null));
