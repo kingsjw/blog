c0ee08ddd0a32d8c2853bc70b6a3d750fd8baac7
kingsjw <kingsjw7@gmail.com>
2023-12-20 23:43:49 +0900

    빌드 파일 배포

--- docs/_nuxt/static/1703083162/travel/fukuoka1905/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1703083162",layout:"default",error:a,state:{device:{isMobile:false}},serverRendered:true,routePath:"\u002Ftravel\u002Ffukuoka1905",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6a93d7e2"}}}}(null));
