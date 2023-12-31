572e07418226c037efbde9d8b88f1892675fafa9
kingsjw <kingsjw7@gmail.com>
2023-12-31 17:00:43 +0900

    빌드 파일 배포

--- docs/_nuxt/static/1703418587/movie/catcheMeIfYouCan/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1703418587",layout:"default",error:a,state:{device:{isMobile:false}},serverRendered:true,routePath:"\u002Fmovie\u002FcatcheMeIfYouCan",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2f66cc35"}}}}(null));
